5a5cba51cc94fa38e11edd9d33f650900ba7cbe5
Brad Carter <bradc1993@gmail.com>
2020-05-20 00:05:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
-  original_sentence = sentence.split
+  original_sentence = sentence.split(" ")
   new_sentence = []
   original_sentence.each do |word|
     new_sentence << word.reverse 
   end
-  new_sentence
+  new_sentence.join(" ")
 end
   
