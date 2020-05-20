e0221c14e154cfd5a5ffbfb4f11557a1d35537d1
Brad Carter <bradc1993@gmail.com>
2020-05-20 00:14:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,8 @@
 def reverse_each_word(sentence)
   original_sentence = sentence.split(" ")
   new_sentence = []
-  original_sentence.each do |word|
-    new_sentence << word.reverse 
+  original_sentence.collect do |word|
+    new_sentence << word.reverse
   end
   new_sentence.join(" ")
-end
-  
-def reverse_each_word(sentence)
-  sentence.!split(" ").collect do |word|
-    word.reverse
-  end
-  sentence.join(" ")
 end
